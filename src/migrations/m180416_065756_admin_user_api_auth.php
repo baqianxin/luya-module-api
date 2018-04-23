1bb3bfcd1a2e6eb5a0ab44aa12e55c189f145714
oom <baqianxin@163.com>
2018-04-23 22:14:18 +0800

    aler table strac

--- src/migrations/m180416_065756_admin_user_api_auth.php
+++ src/migrations/m180416_065756_admin_user_api_auth.php
@@ -5,9 +5,9 @@ use yii\db\Migration;
 /**
  * Class m180416_065756_api_auth_user
  */
-class m180416_065756_api_auth_user extends Migration
+class m180416_065756_admin_user_api_auth extends Migration
 {
-    public $userTable = 'api_auth_user';
+    public $userTable = 'admin_user_api_auth';
 
     /**
      * {@inheritdoc}
@@ -23,8 +23,6 @@ class m180416_065756_api_auth_user extends Migration
         if ($this->db->schema->getTableSchema($this->userTable, true) === null) {
             $this->createTable($this->userTable, [
                 'id' => $this->primaryKey(),
-                'email' => $this->string()->notNull(),
-                'username' => $this->string(32)->notNull(),
                 'app_key' => $this->string(32)->notNull(),
                 'api_token' => $this->string()->notNull(),
                 'app_secret' => $this->string()->notNull(),
@@ -35,6 +33,17 @@ class m180416_065756_api_auth_user extends Migration
                 'created_at' => $this->integer()->notNull(),
                 'updated_at' => $this->integer()->notNull(),
             ], $tableOptions);
+            /**
+                参数1：设置本表的外键名（可自定义）。
+                参数2：本表表名。
+                参数3：本表中与外表关联的字段名称。如果有多个字段，以逗号分隔或使用一个数组。
+                参数4：外表表名。
+                参数5：外表中与本表关联的字段名称。如果有多个字段，以逗号分隔或使用一个数组。
+                参数6：删除选项，可选，默认为空。可选的类型有 RESTRICT, CASCADE, NO ACTION, SET DEFAULT, SET NULL。
+                参数7：更新选项，可选，默认为空。可选的类型有 RESTRICT, CASCADE, NO ACTION, SET DEFAULT, SET NULL。
+                注意：参数3和参数5的字段类型必须一致，否则执行失败。
+             */
+            $this->addForeignKey('id_admin',$this->userTable,'id','admin_user','id');
         }
     }
     public function down()
